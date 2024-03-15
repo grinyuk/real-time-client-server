425efc01b46dfe35abe29c48e9b9cca639b80677
grinyuk <grinyuk309@gmail.com>
2024-03-16 00:06:03 +0200

    added front

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.css';
+import LongPolling from "./LongPolling";
+
+const App = () => {
+    return (
+        <div>
+            <LongPolling />
+        </div>
+    );
+};
+
+export default App;
